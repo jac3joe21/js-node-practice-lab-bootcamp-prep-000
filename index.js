8087752ba2c641f53d33c4ab4d5d2d14e091abc6
Joseph Casiano <josephcasiano@gmail.com>
2017-11-02 19:43:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Joseph"
-var height = "78"
+var height = 78
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
